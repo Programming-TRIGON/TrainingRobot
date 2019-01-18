99bba712a393f561b9caa17cb852d94ffe334a47
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-01-18 11:54:51 +0200

    last changes from 17.1

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -8,6 +8,9 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.TrackVisionTarget;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -15,8 +18,10 @@ import edu.wpi.first.wpilibj.XboxController;
  */
 public class OI {
   public XboxController xbox;
+  Button XButton = new JoystickButton(this.xbox, 1);
   public OI(){
     // TODO: change to robotMap port
     this.xbox = new XboxController(0);
+    XButton.whileHeld(TrackVisionTarget());
   }
 }
