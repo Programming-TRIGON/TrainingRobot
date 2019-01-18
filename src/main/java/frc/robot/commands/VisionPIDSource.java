ef79ece31399b4832e3d7770494976c50c168e61
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-01-18 14:08:06 +0200

    switch for target listening

--- src/main/java/frc/robot/commands/VisionPIDSource.java
+++ src/main/java/frc/robot/commands/VisionPIDSource.java
@@ -7,12 +7,18 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDSourceType;
 
 public class VisionPIDSource implements PIDSource {
-    public VisionPIDSource(){
-        
+    
+    VisionTarget target;
+
+    public VisionPIDSource(VisionTarget target){
+        this.target=target;
     } 
 	@Override
 	public void setPIDSourceType(PIDSourceType pidSource) {
@@ -21,13 +27,30 @@ public class VisionPIDSource implements PIDSource {
 
 	@Override
 	public PIDSourceType getPIDSourceType() {
-		return null;
+		return PIDSourceType.kDisplacement;
 	}
 
 	@Override
 	public double pidGet() {
 		return 0;
 	}
-    
 
+    public double runListenerNetworkTable(){
+        NetworkTableInstance inst = NetworkTableInstance.getDefault();
+         // TODO: find which table we are using to upload vision target dircetion
+        NetworkTable TargetTable = inst.getTable("VisionTable");
+        String TargetKey;
+        switch (VisionTarget){
+            case VisionTarget.kRetroflector: TargetKey = "hatchDirection"; break;
+            case VisionTarget.kCargo: TargetKey = "hatchDirection"; break;
+            case VisionTarget.kLine: TargetKey = "hatchDirection"; break;
+            default: VisionTarget.kHatch TargetKey = "hatchDirection"; break;
+        }
+        NetworkTableEntry TargetEntry = TargetTable.getEntry(TargetKey);
+        return
+    }
+
+    public static enum VisionTarget {
+        kHatch, kCargo, kRetroflector, kLine;
+      }
 }
