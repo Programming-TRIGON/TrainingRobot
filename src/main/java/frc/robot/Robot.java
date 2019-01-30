aca7053da677ce51b65ccf44ea7d74dfc69e085a
Moshe Chelsky <Moshechelsky@gmail.com>
2019-01-30 21:45:22 +0200

    started motion profiling
    starting in bold letters

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,66 +1,40 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.VictorSP;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.DriveTrain;
+import jaci.pathfinder.followers.EncoderFollower;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
   public static DriveTrain driveTrain;
   public static OI m_oi;
 
-
-
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-
   @Override
   public void robotInit() {
-    Robot.driveTrain = new DriveTrain(
-      new VictorSP(RobotMap.BACK_LEFT_MOTOR),new VictorSP(RobotMap.FRONT_LEFT_MOTOR), 
-      new VictorSP(RobotMap.BACK_RIGHT_MOTOR),new VictorSP(RobotMap.FRONT_RIGHT_MOTOR), 
-      new Encoder(RobotMap.ENCODER_LEFT_A, RobotMap.ENCODER_LEFT_B), 
-      new Encoder(RobotMap.ENCODER_RIGHT_A, RobotMap.ENCODER_RIGHT_B));
+    Robot.driveTrain = new DriveTrain(new VictorSP(RobotMap.BACK_LEFT_MOTOR), new VictorSP(RobotMap.FRONT_LEFT_MOTOR),
+        new VictorSP(RobotMap.BACK_RIGHT_MOTOR), new VictorSP(RobotMap.FRONT_RIGHT_MOTOR),
+        new Encoder(RobotMap.ENCODER_LEFT_A, RobotMap.ENCODER_LEFT_B),
+        new Encoder(RobotMap.ENCODER_RIGHT_A, RobotMap.ENCODER_RIGHT_B), new AnalogGyro(RobotMap.ANALOG_GYRO),
+        new EncoderFollower(PathfinderFRC.getTrajectory(PathfinderConstants.PathName + ".left"),
+            new EncoderFollower(PathfinderFRC.getTrajectory(PathfinderConstants.PathName + ".right"))),
+        new Notifier(this::followPath));
     m_oi = new OI();
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {
     SmartDashboard.putBoolean("Drivetrain Inverted", Robot.driveTrain.isInverted());
   }
 
   /**
-   * This function is called once each time the robot enters Disabled mode.
-   * You can use it to reset any subsystem information you want to clear when
-   * the robot is disabled.
+   * This function is called once each time the robot enters Disabled mode. You
+   * can use it to reset any subsystem information you want to clear when the
+   * robot is disabled.
    */
   @Override
   public void disabledInit() {
@@ -73,26 +47,26 @@ public class Robot extends TimedRobot {
 
   /**
    * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString code to get the auto name from the text box below the Gyro
+   * between different autonomous modes using the dashboard. The sendable chooser
+   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+   * remove all of the chooser code and uncomment the getString code to get the
+   * auto name from the text box below the Gyro
    *
-   * <p>You can add additional auto modes by adding additional commands to the
-   * chooser code above (like the commented example) or additional comparisons
-   * to the switch structure below with additional strings & commands.
+   * <p>
+   * You can add additional auto modes by adding additional commands to the
+   * chooser code above (like the commented example) or additional comparisons to
+   * the switch structure below with additional strings & commands.
    */
   @Override
   public void autonomousInit() {
 
     /*
-     * String autoSelected = SmartDashboard.getString("Auto Selector",
-     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-     * = new MyAutoCommand(); break; case "Default Auto": default:
-     * autonomousCommand = new ExampleCommand(); break; }
+     * String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
+     * switch(autoSelected) { case "My Auto": autonomousCommand = new
+     * MyAutoCommand(); break; case "Default Auto": default: autonomousCommand = new
+     * ExampleCommand(); break; }
      */
 
-    
   }
 
   /**
@@ -109,7 +83,7 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-    
+
   }
 
   /**
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -5,65 +5,75 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.subsystems ;
+package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.PathfinderConstants;
 import frc.robot.Robot;
 import frc.robot.commands.DriveJoystick;
-
+import jaci.pathfinder.PathfinderFRC;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.followers.EncoderFollower;
 
 public class DriveTrain extends Subsystem {
   SpeedController backLeftMotor, frontLeftMotor, backRightMotor, frontRightMotor;
   Encoder encoderRight, encoderLeft;
- // Gyro gyro;
+  AnalogGyro gyro;
+  EncoderFollower encoderFollowerLeft, encoderFollowerRight;
+  Notifier encoderNotifier;
   DifferentialDrive differentialDrive;
   boolean inverted = false;
 
-public DriveTrain(
-  SpeedController rightBack, SpeedController rightFront, 
-  SpeedController leftBack, SpeedController leftFront, 
-  Encoder encoderRight, Encoder encoderLeft) {
-  this.backRightMotor = rightBack;
-  this.frontRightMotor = rightFront;
-  this.backLeftMotor = leftBack;
-  this.frontLeftMotor = leftFront;
-  this.encoderRight = encoderRight;
-  this.encoderLeft  = encoderLeft;
+  public DriveTrain(SpeedController rightBack, SpeedController rightFront, SpeedController leftBack,
+      SpeedController leftFront, Encoder encoderRight, Encoder encoderLeft, AnalogGyro gyro,
+      EncoderFollower encoderFollowerRight, EncoderFollower encoderFollowerLeft, Notifier encoderNotifier) {
+    this.backRightMotor = rightBack;
+    this.frontRightMotor = rightFront;
+    this.backLeftMotor = leftBack;
+    this.frontLeftMotor = leftFront;
+    this.encoderRight = encoderRight;
+    this.encoderLeft = encoderLeft;
+    this.gyro = gyro;
+    this.encoderFollowerRight = encoderFollowerRight;
+    this.encoderFollowerLeft = encoderFollowerLeft;
+    this.encoderNotifier = encoderNotifier;
 
-  SpeedControllerGroup rightMotorGroup = new SpeedControllerGroup(backRightMotor, frontRightMotor);
-  SpeedControllerGroup leftMotorGroup = new SpeedControllerGroup(backLeftMotor, frontLeftMotor);
-  this.differentialDrive = new DifferentialDrive(leftMotorGroup, rightMotorGroup);
- // this.gyro = gyro;
+    SpeedControllerGroup rightMotorGroup = new SpeedControllerGroup(backRightMotor, frontRightMotor);
+    SpeedControllerGroup leftMotorGroup = new SpeedControllerGroup(backLeftMotor, frontLeftMotor);
+    this.differentialDrive = new DifferentialDrive(leftMotorGroup, rightMotorGroup);
+  }
 
-
-}
-  public void arcadeDrive(double x, double y){
+  public void arcadeDrive(double x, double y) {
     this.differentialDrive.arcadeDrive(x, y);
   }
 
-  public double encRightTicks(){
+  public double encRightTicks() {
     return encoderRight.get();
   }
 
-  public double encLeftTicks(){
+  public double encLeftTicks() {
     return encoderLeft.get();
-  } 
+  }
 
- /* public double gyroGet(){
-    return gyro.getRate();
-  } */ 
+  /*
+   * public double gyroGet(){ return gyro.getRate(); }
+   */
   @Override
   public void initDefaultCommand() {
     setDefaultCommand(new DriveJoystick(Robot.m_oi.xbox));
   }
-  public void toggleInverted(){
+
+  public void toggleInverted() {
     this.inverted = !this.inverted;
   }
-  public boolean isInverted(){
+
+  public boolean isInverted() {
     return this.inverted;
   }
 }
